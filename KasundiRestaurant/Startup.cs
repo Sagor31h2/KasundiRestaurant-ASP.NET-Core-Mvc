a06740282c8d6a8c35acd0bca9ac6170fb03616b
Sagor31h2 <sagor31h2@gmail.com>
2020-05-30 14:05:03 +0600

    Adding RuntimeCompilaton

--- KasundiRestaurant/Startup.cs
+++ KasundiRestaurant/Startup.cs
@@ -33,7 +33,7 @@ namespace KasundiRestaurant
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
