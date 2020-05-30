fa50bf64ce9e78295e3499509b9731e69124441b
Sagor31h2 <sagor31h2@gmail.com>
2020-05-30 14:44:44 +0600

    Add category controller and view

---
+++ KasundiRestaurant/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KasundiRestaurant.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KasundiRestaurant.Areas.Admin.Controllers
+{[Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        //GET
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Category.ToListAsync());
+        }
+    }
+}
