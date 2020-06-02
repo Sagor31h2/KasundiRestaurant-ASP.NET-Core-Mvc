4edfc7e442b1c45bed53c23cb2593639c8a26847
Sagor31h2 <sagor31h2@gmail.com>
2020-06-02 21:18:19 +0600

    Add SubCategory Index action and view

---
+++ KasundiRestaurant/Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KasundiRestaurant.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KasundiRestaurant.Areas.Admin.Controllers
+{[Area("Admin")]
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var subCategory = await _db.SubCategory.Include(m => m.Category).ToListAsync();
+            return View(subCategory);
+        }
+    }
+}
