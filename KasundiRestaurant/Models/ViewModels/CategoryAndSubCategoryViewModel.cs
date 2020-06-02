6b31b770738170bcdfebaf567559fce640bcf667
Sagor31h2 <sagor31h2@gmail.com>
2020-06-02 21:25:31 +0600

    Add CataegoryAndSubCategoryViewModel

---
+++ KasundiRestaurant/Models/ViewModels/CategoryAndSubCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KasundiRestaurant.Models.ViewModels
+{
+    public class CategoryAndSubCategoryViewModel
+    {
+        public IEnumerable<Category> CategoryList { get; set; }
+        public SubCategory SubCategory { get; set; }
+        public List<SubCategory> SubCategoryList { get; set; }
+    }
+}
