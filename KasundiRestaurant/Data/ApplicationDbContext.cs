467a29d7f3eed9f553fcd1ebb1e1ce61467a7a47
Sagor31h2 <sagor31h2@gmail.com>
2020-06-07 00:02:06 +0600

    Add MenuItem model

--- KasundiRestaurant/Data/ApplicationDbContext.cs
+++ KasundiRestaurant/Data/ApplicationDbContext.cs
@@ -16,5 +16,6 @@ namespace KasundiRestaurant.Data
 
         public DbSet<Category> Category { get; set; }
         public DbSet<SubCategory> SubCategory { get; set; }
+        public DbSet<MenuItem> MenuItem { get; set; }
     }
 }
---
+++ KasundiRestaurant/Models/MenuItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KasundiRestaurant.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Price should be greater Than ${1}")]
+        public double Price { get; set; }
+
+        public string Spicyness { get; set; }
+        public enum Espicy
+        {
+            Na = 0,
+            NotSpicy = 1,
+            Spicy = 2,
+            VerySpicy = 3
+        }
+
+        public string Image { get; set; }
+
+        [Display(Name = "CategoryId")]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")] public virtual Category Category { get; set; }
+
+
+        [Display(Name = "SubCategoryId")]
+        public int SubCategoryId { get; set; }
+
+        [ForeignKey("SubCategoryId")] public virtual SubCategory SubCategory { get; set; }
+    }
+}
