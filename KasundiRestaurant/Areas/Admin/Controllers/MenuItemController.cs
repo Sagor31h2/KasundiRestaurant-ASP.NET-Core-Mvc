fdec43df5bdf779e5e5baac92be8d1fc560ec215
sagor31h2 <sagor31h2@gmail.com>
2020-06-08 12:27:55 +0600

    Menu Item Index Get and View

---
+++ KasundiRestaurant/Areas/Admin/Controllers/MenuItemController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KasundiRestaurant.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KasundiRestaurant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public MenuItemController(ApplicationDbContext db,IWebHostEnvironment hostEnvironment)
+        {
+            _db = db;
+            _hostEnvironment = hostEnvironment;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var menuItems = await _db.MenuItem.
+                Include(c=>c.Category).
+                Include(c=>c.SubCategory).ToListAsync();
+            return View(menuItems);
+        }
+    }
+}
