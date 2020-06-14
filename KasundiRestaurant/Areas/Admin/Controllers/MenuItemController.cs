f74d45d3946a53ace235ca92cded2c8769ab1662
sagor31h2 <sagor31h2@gmail.com>
2020-06-14 20:40:22 +0600

    Create menu item post

--- KasundiRestaurant/Areas/Admin/Controllers/MenuItemController.cs
+++ KasundiRestaurant/Areas/Admin/Controllers/MenuItemController.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using KasundiRestaurant.Data;
 using KasundiRestaurant.Models;
 using KasundiRestaurant.Models.ViewModels;
+using KasundiRestaurant.Utility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace KasundiRestaurant.Areas.Admin.Controllers
 {
@@ -42,5 +45,58 @@ namespace KasundiRestaurant.Areas.Admin.Controllers
         {
             return View(MenuItemVM);
         }
+
+        //POST-CREATE
+        [ActionName("Create")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePost()
+        {
+            MenuItemVM.MenuItem.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
+
+            if (!ModelState.IsValid)
+            {
+                return View(MenuItemVM);
+            }
+
+            _db.MenuItem.Add(MenuItemVM.MenuItem);
+            await _db.SaveChangesAsync();
+
+            //image saving
+            string webRootPath = _hostEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+
+            var menuItemFormDb = await _db.MenuItem.FindAsync(MenuItemVM.MenuItem.Id);
+
+            if (files.Count>0)
+            {
+               //file uploaded 
+               var uploads = Path.Combine(webRootPath, "Images");
+               var extension = Path.GetExtension(files[0].FileName);
+
+               using (var fileStream=new FileStream(Path.Combine(uploads,MenuItemVM.MenuItem.Id+extension),FileMode.Create))
+               {
+                   files[0].CopyTo(fileStream);
+
+               }
+
+               menuItemFormDb.Image = @"\Images\" + MenuItemVM.MenuItem.Id + extension;
+            }
+
+            else
+            {
+                //no file uploaded
+                var uploads = Path.Combine(webRootPath, @"Images\" + StaticDetails.DefaultFoodImage);
+                System.IO.File.Copy(uploads, webRootPath + @"\Images\" + MenuItemVM.MenuItem.Id + ".png");
+                menuItemFormDb.Image = @"\Images\" + MenuItemVM.MenuItem.Id + ".png";
+            }
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
---
+++ KasundiRestaurant/Utility/StaticDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KasundiRestaurant.Utility
+{
+    public static class StaticDetails
+    {
+        public const string DefaultFoodImage = "default_food.png";
+    }
+}
