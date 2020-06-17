1735ac867a85efcefd32b9cf7beeae90d577b7f8
sagor31h2 <sagor31h2@gmail.com>
2020-06-17 23:08:24 +0600

    Coupon Index

---
+++ KasundiRestaurant/Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KasundiRestaurant.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KasundiRestaurant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CouponController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CouponController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var couponList = await _db.Coupon.ToListAsync();
+
+            return View(couponList);
+        }
+    }
+}
--- KasundiRestaurant/Data/ApplicationDbContext.cs
+++ KasundiRestaurant/Data/ApplicationDbContext.cs
@@ -17,5 +17,6 @@ namespace KasundiRestaurant.Data
         public DbSet<Category> Category { get; set; }
         public DbSet<SubCategory> SubCategory { get; set; }
         public DbSet<MenuItem> MenuItem { get; set; }
+        public DbSet<Coupon> Coupon { get; set; }
     }
 }
