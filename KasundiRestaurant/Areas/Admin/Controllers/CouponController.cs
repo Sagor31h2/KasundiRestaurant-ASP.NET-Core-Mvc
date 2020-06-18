27f2012461aed2ea3e2b288b376ad4bd90e7c56c
sagor31h2 <sagor31h2@gmail.com>
2020-06-18 23:35:18 +0600

    Create coupon

--- KasundiRestaurant/Areas/Admin/Controllers/CouponController.cs
+++ KasundiRestaurant/Areas/Admin/Controllers/CouponController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using KasundiRestaurant.Data;
+using KasundiRestaurant.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,5 +25,41 @@ namespace KasundiRestaurant.Areas.Admin.Controllers
 
             return View(couponList);
         }
+
+        //GET-COUPON
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Coupon coupons)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count>0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1=files[0].OpenReadStream())
+                    {
+                        using (var ms1=new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+
+                    coupons.Picture = p1;
+                }
+                _db.Coupon.Add(coupons);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+
     }
 }
