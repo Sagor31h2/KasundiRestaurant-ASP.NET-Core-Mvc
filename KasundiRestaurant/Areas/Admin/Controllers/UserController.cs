195052c5fbf6b4e093ae9d332a741492f099c7c0
sagor31h2 <sagor31h2@gmail.com>
2020-07-03 23:13:00 +0600

    Users Controller and Index

---
+++ KasundiRestaurant/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using KasundiRestaurant.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KasundiRestaurant.Areas.Admin.Controllers
+{[Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var claimsIdentity = (ClaimsIdentity) this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            return View(await _db.ApplicationUser.Where(c=>c.Id!=claim.Value).ToListAsync());
+        }
+    }
+}
